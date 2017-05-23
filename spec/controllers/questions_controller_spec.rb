440ed488450ce992ea48774b4e65b5e95dd4f627
aleksposte <aleks.poste@gmail.com>
2017-05-23 13:15:43 +0600

    PR 1

---
+++ spec/controllers/questions_controller_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe QuestionsController, type: :controller do
+  describe 'GET #index' do
+    it 'populates an array of all questions' do
+      question1 = FactoryGirl.create(:question)
+      question2 = FactoryGirl.create(:question)
+
+      get :index
+
+      expect(assigns(:questions)).ti match_array([question1, question2])
+    end
+
+    it 'renders index view' do
+      get :index
+      expect(responce).to render_template :index
+    end
+  end
+end
