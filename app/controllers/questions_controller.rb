ba1fc6d8765914848d8373879bf7b089139c4ceb
aleksposte <aleks.poste@gmail.com>
2017-07-12 13:00:46 +0600

    add upload files to question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -11,13 +11,9 @@ class QuestionsController < ApplicationController
     @answer = @question.answers.build
   end
 
-  # def show
-  #   @answer = @question.answers.new
-  #   @answer.attachments.build
-  # end
-
   def new
     @question = Question.new
+    @question.attachments.build
   end
 
   def create
@@ -47,6 +43,6 @@ class QuestionsController < ApplicationController
   end
 
   def question_params
-    params.require(:question).permit(:title, :body)
+    params.require(:question).permit(:title, :body, attachments_attributes: [:id, :file, :destoy])
   end
 end
