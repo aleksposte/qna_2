845d35a00e2dd1be8ab6a0eeb0c9b24e8fb64695
aleksposte <aleks.poste@gmail.com>
2017-07-14 16:29:55 +0600

    add attachments controller

--- spec/factories/attachments.rb
+++ spec/factories/attachments.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :attachment do
-    file "MyString"
+    # file "MyString"
+    file Rack::Test::UploadedFile.new(File.open("#{Rails.root}/spec/spec_helper.rb"))
   end
 end
